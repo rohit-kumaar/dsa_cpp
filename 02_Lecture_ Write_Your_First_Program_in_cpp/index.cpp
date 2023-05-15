5184c4b5d602645de5990adcf494fbbabf43a889
Rohit Kumar <rohit96682@gmail.com>
2023-05-15 20:49:11 +0530

    up to variable conversion

---
+++ 02_Lecture_ Write_Your_First_Program_in_cpp/index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+}
