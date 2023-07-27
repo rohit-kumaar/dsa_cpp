04d8fac284a5f875c405d5fd18aff52669630d4d
Rohit Kumar <rohit96682@gmail.com>
2023-07-27 22:05:31 +0530

    initial commit : Binary & Decimal Number System

---
+++ 06_Binary_&_Decimal_Number_System/index.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 3;
+    cout << a << endl;
+
+    if (true)
+    {
+        int a = 5;
+        cout << a << endl;
+    }
+}
