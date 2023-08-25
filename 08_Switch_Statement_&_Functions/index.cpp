0881e78e9694b1befea2692d3d9f86891eda503e
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-08-25 16:37:23 +0530

    initial commit

---
+++ 08_Switch_Statement_&_Functions/index.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+using namespace std;
+
+/*
+Calculator
+ */
+// int main()
+// {
+
+//     int a, b;
+
+//     cout << "Enter the value of a" << endl;
+//     cin >> a;
+
+//     cout << "Enter the value of b" << endl;
+//     cin >> b;
+
+//     char op;
+//     cout << "Enter the operation you want to perform" << endl;
+//     cin >> op;
+
+//     switch (op)
+//     {
+
+//     case '+':
+//         cout << "Addition is : " << (a + b) << endl;
+//         break;
+
+//     case '-':
+//         cout << "Subtraction is : " << (a - b) << endl;
+//         break;
+
+//     case '/':
+//         cout << "Division is : " << (a / b) << endl;
+//         break;
+
+//     case '*':
+//         cout << "Multiplication is : " << (a * b) << endl;
+//         break;
+
+//     case '%':
+//         cout << "Reminder is : " << (a % b) << endl;
+//         break;
+
+//     default:
+//         cout << "Please Add Operation You Want To Perform!";
+//         break;
+//     }
+// }
+
+/*
+Find - 100, 50, 20, 1
+*/
+int main()
+{
+
+    int given = 1330;
+    cout << (given / 100);
+}
